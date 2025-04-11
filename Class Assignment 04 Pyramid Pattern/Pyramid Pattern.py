5f9ee8848015c93373966b4e767846034e8c8c9b
Hasan Rafay <160109106+Hasan9060@users.noreply.github.com>
2025-04-11 22:15:57 +0500

    Diamond Pattern.py to Pyramid Pattern.py

--- Class Assignment 04 Pyramid Pattern/Pyramid Pattern.py
+++ Class Assignment 04 Pyramid Pattern/Pyramid Pattern.py
@@ -4,6 +4,3 @@ nor = 5  # nor means number of rows
 for i in range(1, nor + 1):
     print(" " * (nor - i) + "*" * (2 * i - 1))
 
-#Bottom pyramid
-for i in range(1, nor):
-    print(" " * i + "*" * (2 * (nor - i) - 1))
